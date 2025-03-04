4defd03416b5ac19503817e920543064751d7faf
d5avard <dany.savard@gmail.com>
2025-03-04 08:17:14 -0500

    add requester server first draft add a architecture design for the solution

---
+++ cmd/requester/requester.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	response := Response{Message: "Hello, World!"}
+	json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	port := ":8080"
+	log.Printf("Starting server on %s", port)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatalf("Could not start server: %s", err)
+	}
+}
