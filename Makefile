a0290b743fba286ba58650c3d0975e05eb10b76e
d5avard <dany.savard@gmail.com>
2025-04-02 19:21:24 -0400

    Add an entrypoint / Add template home.html Restructure application for using templates

--- Makefile
+++ Makefile
@@ -1,14 +1,28 @@
+version := $(shell cat ./deployments/web/version.txt)
+
 build-question:
 	go build -o question ./cmd/question/.
 
 build-models:
 	go build -o models ./cmd/models/.
 
-migration_up:
+migration-up:
 	migrate -database ${POSTGRESQL_URL} -path database/migration -verbose up
 
-migration_down:
+migration-down:
 	migrate -database ${POSTGRESQL_URL} -path database/migration -verbose down
 
-migration_fix:
+migration-fix:
 	migrate -database ${POSTGRESQL_URL} -path database/migration/ force VERSION
+
+web-build:
+	docker build -t d5avard/web:$(version) -f ./deployments/web/Dockerfile .
+
+web-push:
+	docker push d5avard/web:$(version)
+
+web-run-local:
+	go run ./cmd/web/web.go --host localhost --port 8080 --templatePath "./web/web/templates"
+
+web-run-debug:
+	docker run -d --user webuser -p 8080:80 d5avard/$(version)
--- cmd/web/web.go
+++ cmd/web/web.go
@@ -3,26 +3,33 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 
+	"github.com/d5avard/factory/internal"
 	"github.com/gorilla/mux"
 	"github.com/spf13/cobra"
 )
 
+var host string
 var port string
+var templatePath string
+var staticPath string
 
 var rootCmd = &cobra.Command{
 	Use:   "web",
 	Short: "Run the web HTTP server",
 	Run: func(cmd *cobra.Command, args []string) {
 		router := mux.NewRouter()
+		router.HandleFunc("/", homeHandler).Methods("GET")
 		router.HandleFunc("/status", statusHandler).Methods("GET")
 		http.Handle("/", router)
 
 		log.Printf("Starting server on %s", port)
 
-		addr := fmt.Sprintf("0.0.0.0:%s", port)
+		addr := fmt.Sprintf("%s:%s", host, port)
 		if err := http.ListenAndServe(addr, nil); err != nil {
 			log.Fatalf("Could not start server: %s", err)
 		}
@@ -30,12 +37,42 @@ var rootCmd = &cobra.Command{
 }
 
 func init() {
+	rootCmd.Flags().StringVar(&host, "host", "0.0.0.0", "Host to bind the server to")
 	rootCmd.Flags().StringVar(&port, "port", "80", "Port to run the server on")
+	rootCmd.Flags().StringVar(
+		&templatePath,
+		"templatePath",
+		"./templates",
+		"Path to the HTML template")
+	rootCmd.Flags().StringVar(
+		&staticPath,
+		"staticPath",
+		"./static",
+		"Path to the static files")
 }
 
 func statusHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]string{"status": "OK"})
+	internal.LogRequest(r, http.StatusText(http.StatusOK), http.StatusOK)
+}
+
+const homeFile = "home.html"
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	file := filepath.Join(templatePath, homeFile)
+	tmpl, err := template.ParseFiles(file)
+	if err != nil {
+		internal.HttpError(w, r, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err = tmpl.Execute(w, nil); err != nil {
+		internal.HttpError(w, r, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	internal.LogRequest(r, http.StatusText(http.StatusOK), http.StatusOK)
 }
 
 func main() {
